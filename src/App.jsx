d40cd68b7a9e98786da5057253d879d18a54e09c
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-04 19:52:51 -0700

    added local "api" for questions. added reducer to handle initial actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
 import Header from "./components/Header";
-import DateCounter from "./components/DateCounter";
+import Main from "./components/Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "error":
+      return { ...state, status: "error" };
+    case "loading":
+      return { ...state, status: "loading" };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:8000/questions");
+        const data = await response.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "error", payload: error });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>{}</Main>
     </div>
   );
 }
